588e75e4dedf448a0700a625df9e88879d2a3eb5
AbdoEl7alaby <67608921+AbdoEl7alaby@users.noreply.github.com>
2023-11-13 06:38:40 +0200

    Update wordle.py

--- wordle.py
+++ wordle.py
@@ -1,23 +1,28 @@
-word = "hello"
+import random
+print("================================")
+print("|  O = right letter and place  |\n| C = right letter wrong place |\n|  X = wrong letter and place  |\n|        ! # NO CAPS #!        |")
+print("================================")
+words = ['happy','water','house','juice','power','knife','hello','penta','apple','horse','flash','glass','board','creed','mouse','cable','sound','speed','spoon','fight','queen','novel','black','width','eight','plane','virus','right','teeth','tower','drama']
 answer = ""
-X = "X"
-O = "O"
-C = "C"
-for j in range (5):
-    guess = input("Enter your guess")
-    nguess = guess.lower()
-    for i in range (len(nguess)) :
-        if nguess[i] in word :
-            if nguess[i] == word[i] :
-                answer += "O"
+for k in range (len(words)):
+    word = random.choice(words)
+    for j in range (5):
+        guess = input("Enter your guess : ")
+        nguess = guess.lower()
+        for i in range (len(nguess)) :
+            if nguess[i] in word :
+                if nguess[i] == word[i] :
+                    answer += "O "
+                else :
+                    answer += "C "
             else :
-                answer += "C"
+                    answer += "X "
+        print(answer)
+        if answer == "O O O O O ":
+            print("Congrates u got it\n------------------------------")
+            answer = ""
+            break
         else :
-                answer += "X"
-    print(answer)
-    if answer == "OOOOO":
-        print("Congrates u got it")
-        break
-    else :
-        print("Try again")
-        answer = ""
+            print("Try again")
+            answer = ""
+print("THE END")
